c0dd1e6988d199f03a295344ab01ec40d00c9654
BrianGaray <brianchess18@gmail.com>
2022-05-02 20:02:01 -0500

    Se añade un Nav con titulo "consultar clima"

--- app-clima/src/App.js
+++ app-clima/src/App.js
@@ -1,10 +1,11 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hola mundo</h1>
+      <NavBar />
+    
     </div>
   );
 }
