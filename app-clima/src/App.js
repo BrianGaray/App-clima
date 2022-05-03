fbdf887ef2fa0bf59fad436e61141f511709577a
BrianGaray <brianchess18@gmail.com>
2022-05-02 22:51:11 -0500

    Se crea el componente WeatherPanel, para extraer los datos de la API

--- app-clima/src/App.js
+++ app-clima/src/App.js
@@ -1,11 +1,13 @@
-import './App.css';
+import './assets/css/App.css';
 import NavBar from './components/NavBar';
+import WeatherPanel from './components/WeatherPanel';
 
 function App() {
   return (
     <div className="App">
       <NavBar />
-    
+      <WeatherPanel />
+
     </div>
   );
 }
