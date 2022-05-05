fe995eca6e90b6673f33af3b7095b5b5978f2ed7
BrianGaray <brianchess18@gmail.com>
2022-05-05 14:06:34 -0500

    Ultimos detalles de estilos.

--- app-clima/src/components/Card.js
+++ app-clima/src/components/Card.js
@@ -24,11 +24,11 @@ const Card = ({loadingData, showData, weather, forecast}) => {
         url = "http://openweathermap.org/img/w/";
         iconUrl = url + weather.weather[0].icon + ".png";
       
-        iconUrl3 = url + forecast.list[1].weather[0].icon + ".png";
-        iconUrl6 = url + forecast.list[2].weather[0].icon + ".png";
+        iconUrl3 = url + forecast.list[0].weather[0].icon + ".png";
+        iconUrl6 = url + forecast.list[1].weather[0].icon + ".png";
        
-        forecastDate3 = forecast.list[1].dt_txt.substring(8, 10) + '/' + forecast.list[1].dt_txt.substring(5, 7) + '/' + forecast.list[1].dt_txt.substring(0, 4) + ' ' +  forecast.list[1].dt_txt.substring(11, 13);
-        forecastDate6 = forecast.list[2].dt_txt.substring(8, 10) + '/' + forecast.list[2].dt_txt.substring(5, 7) + '/' + forecast.list[2].dt_txt.substring(0, 4) + ' ' +  forecast.list[2].dt_txt.substring(11, 13);
+        forecastDate3 = forecast.list[0].dt_txt.substring(8, 10) + '/' + forecast.list[0].dt_txt.substring(5, 7) + '/' + forecast.list[0].dt_txt.substring(0, 4) + ' ' +  forecast.list[0].dt_txt.substring(11, 13);
+        forecastDate6 = forecast.list[1].dt_txt.substring(8, 10) + '/' + forecast.list[1].dt_txt.substring(5, 7) + '/' + forecast.list[1].dt_txt.substring(0, 4) + ' ' +  forecast.list[1].dt_txt.substring(11, 13);
        
     }
 
